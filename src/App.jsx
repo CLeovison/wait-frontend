a81dfc68b0316eac3ec3f38743f418e6d8a8a427
CLeovison <cleovison@gmail.com>
2024-06-19 00:38:34 +0800

    Added Assets/Logo

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import './App.css'
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+    <h1 className='text-xl text-red-400'>Trial</h1>
+    </>
   )
 }
