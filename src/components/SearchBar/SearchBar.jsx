6b26c523f613b33f8b6eaf11656a5bedc93f95f8
CLeovison <cleovison@gmail.com>
2024-06-22 02:06:45 +0800

    SearchBar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -4,19 +4,22 @@ export default function SearchBar() {
   const [checked, setChecked] = useState(false);
 
  const handleChange = () =>{
+ 
         setChecked(!checked)
  }
 console.log(checked);
   return (
     <>
-      <form >
+      <form className="flex items-center">
         <input
           type="checkbox"
           id="checkbox"
           checked={checked}
           onChange={handleChange}
+          className="hidden"
         />
         <label htmlFor="checkbox"></label>
+        <input type="search" name="" id="" className="outline-none text-slate-700"/>
       </form>
     </>
   );
