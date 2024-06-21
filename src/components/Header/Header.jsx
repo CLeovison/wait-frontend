6537e93ac62875bd5a9e91642d60a50ea71cce64
Leovison <cleovison@gmail.com>
2024-06-21 18:51:03 +0800

    Added Icon

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import { Search } from 'lucide-react'
+import { UserRound } from 'lucide-react'
+import { ShoppingCart } from 'lucide-react'
 export default function Header() {
   return (
     <>
         <nav className='bg-zinc-700 h-20 flex flex-row gap-2 justify-end items-center'>
         
             <ul className='flex flex-row '>
-                <li className='p-4 text-white'><Link >Icon</Link></li>
-                <li className='p-4 text-white'><Link>Icon1</Link></li>
-                <li className='p-4 text-white'><Link>Icon2</Link></li>
-                <li className='p-4 text-white'><Link>Icon3</Link></li>
+                <li className='p-4 text-white'><Link ><Search /></Link></li>
+                <li className='p-4 text-white'><Link><ShoppingCart /></Link></li>
+                <li className='p-4 text-white'><Link><UserRound /></Link></li>
             </ul>
         </nav>
     </>
