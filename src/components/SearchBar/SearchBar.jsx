4218a90156b61384831e2f117fd1bdc16f938c3f
CLeovison <cleovison@gmail.com>
2024-06-24 23:01:25 +0800

    Added A Search Icon In Button

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,17 +1,17 @@
 import React, { useState } from "react";
 import { Search } from "lucide-react";
-
 export default function SearchBar() {
   const [checked, setChecked] = useState(false);
-
-
-
+  // const [isOpen, setIsOpen] = useState(false)
 
   const handleChange = () => {
     setChecked((prev) => !prev);
   };
 
-
+  const handleOpen = () =>{
+    console.log('Trial');
+    
+  }  
   return (
     <>
       <form className="flex items-center">
@@ -21,26 +21,22 @@ export default function SearchBar() {
             <input
               type="checkbox"
               id="search-checkbox"
-              checked={checked}
+              defaultChecked={checked}
               onChange={handleChange}
               className="hidden"
               
             />
             <Search className="cursor-pointer"/>
           </label>
-          
         ) : (
-          
-          <label htmlFor="search">
-        
+          <label htmlFor="search" className="flex ">
             <input
               type="search"
               id="search"
               name=""
-              className="outline-none text-slate-700 "
-              
+              className="outline-none text-slate-700 mr-2  "
             />
-            
+            <Search className="cursor-pointer" onClick={handleOpen}/> 
           </label>
         )}
       </form>
