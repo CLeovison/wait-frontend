62bd91d4d6e6dff24f1fcac15f296e3c684a3c3e
CLeovison <cleovison@gmail.com>
2024-06-25 23:53:27 +0800

    Different Approach on SearchBar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,17 +1,47 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Search } from "lucide-react";
 export default function SearchBar() {
-  const [isHovered, setIsHovered] = useState(false);
-  const [isFocused, setIsFocused] = useState(false);
+  // const targetRef = useRef(null);
+  const [ischecked, setIsChecked] = useState(false);
+  // const [isFocused, setIsFocused] = useState(false);
+  // const showSearchInput = ischecked || isFocused
 
+  // useEffect(() => {
+  //   targetRef.current.value = "";
+  // },[showSearchInput]);
+
+  const handleChange = () => {
+    setIsChecked((prev) => !prev);
+  };
+
+  const handleOpen = () => {
+    console.log("Trial");
+  };
   return (
     <>
       <form className="flex items-center">
-
-        <label htmlFor="search" className="flex">
-          <input type="search" name="" id="" />
-          <Search className="cursor-pointer"/>
-        </label>
+        {!ischecked ? (
+          <label htmlFor="search-checkbox">
+            <input
+              type="checkbox"
+              id="search-checkbox"
+              defaultChecked={ischecked}
+              onChange={handleChange}
+              className="hidden"
+            />
+            <Search className="cursor-pointer" />
+          </label>
+        ) : (
+          <label htmlFor="search" className="flex ">
+            <input
+              type="search"
+              id="search"
+              name=""
+              className="outline-none text-slate-700 mr-2  "
+            />
+            <Search className="cursor-pointer" onClick={handleOpen} />
+          </label>
+        )}
       </form>
     </>
   );
