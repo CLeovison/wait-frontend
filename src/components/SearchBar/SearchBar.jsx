e5f763812b09fa90dbf2ce2d9d536cb2ffa12490
CLeovison <cleovison@gmail.com>
2024-06-25 23:44:33 +0800

    Some Modification On The SearchBar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,44 +1,17 @@
 import React, { useState } from "react";
 import { Search } from "lucide-react";
 export default function SearchBar() {
-  const [checked, setChecked] = useState(false);
-  // const [isOpen, setIsOpen] = useState(false)
+  const [isHovered, setIsHovered] = useState(false);
+  const [isFocused, setIsFocused] = useState(false);
 
-  const handleChange = () => {
-    setChecked((prev) => !prev);
-  };
-
-  const handleOpen = () =>{
-    console.log('Trial');
-    
-  }  
   return (
     <>
       <form className="flex items-center">
-        {!checked ? (
-          <label htmlFor="search-checkbox">
-          
-            <input
-              type="checkbox"
-              id="search-checkbox"
-              defaultChecked={checked}
-              onChange={handleChange}
-              className="hidden"
-              
-            />
-            <Search className="cursor-pointer"/>
-          </label>
-        ) : (
-          <label htmlFor="search" className="flex ">
-            <input
-              type="search"
-              id="search"
-              name=""
-              className="outline-none text-slate-700 mr-2  "
-            />
-            <Search className="cursor-pointer" onClick={handleOpen}/> 
-          </label>
-        )}
+
+        <label htmlFor="search" className="flex">
+          <input type="search" name="" id="" />
+          <Search className="cursor-pointer"/>
+        </label>
       </form>
     </>
   );
