2c19280f448395f2c4822daab00865f85b502567
romeokun <jromevan01@gmail.com>
2024-06-26 19:52:12 +0800

    open search bar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,31 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 //Icons
 
-import { UserRound } from 'lucide-react'
-import { ShoppingCart } from 'lucide-react'
-import SearchBar from '../SearchBar/SearchBar'
+import { UserRound } from "lucide-react";
+import { ShoppingCart } from "lucide-react";
+import SearchBar from "../SearchBar/SearchBar";
 export default function Header() {
-
   return (
     <>
-        <nav className='bg-zinc-700 h-20 flex flex-row gap-2 justify-end items-center'>
-            
-            <ul className='flex'>
-                <li className='p-4 text-white inline-block'><SearchBar /></li>
-                <li className='p-4 text-white inline-block'><Link ><ShoppingCart /></Link></li>
-                <li className='p-4 text-white inline-block'><Link><UserRound /></Link></li>
-            </ul>
-        </nav>
+      <nav className="bg-zinc-700 h-20 flex flex-row gap-2 justify-end items-center">
+        <ul className="flex">
+          <li className="p-4 text-white inline-block">
+            <SearchBar />
+          </li>
+          <li className="p-4 text-white grid content-center">
+            <Link>
+              <ShoppingCart />
+            </Link>
+          </li>
+          <li className="p-4 text-white grid content-center">
+            <Link>
+              <UserRound />
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </>
-  )
+  );
 }
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,47 +1,33 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useRef, useState } from "react";
 import { Search } from "lucide-react";
 export default function SearchBar() {
-  // const targetRef = useRef(null);
-  const [ischecked, setIsChecked] = useState(false);
-  // const [isFocused, setIsFocused] = useState(false);
-  // const showSearchInput = ischecked || isFocused
+  const [isChecked, setIsChecked] = useState(false);
+  const htmlSearchField = useRef(null)
 
-  // useEffect(() => {
-  //   targetRef.current.value = "";
-  // },[showSearchInput]);
-
-  const handleChange = () => {
+  const handleIcon = (e) => {
+    e.preventDefault()
+    if (isChecked && htmlSearchField.current.value.length > 0) {
+      // do search
+      e.target.closest('form').requestSubmit(e.currentTarget)
+    }
     setIsChecked((prev) => !prev);
   };
 
-  const handleOpen = () => {
-    console.log("Trial");
-  };
   return (
     <>
-      <form className="flex items-center">
-        {!ischecked ? (
-          <label htmlFor="search-checkbox">
-            <input
-              type="checkbox"
-              id="search-checkbox"
-              defaultChecked={ischecked}
-              onChange={handleChange}
-              className="hidden"
-            />
-            <Search className="cursor-pointer" />
-          </label>
-        ) : (
-          <label htmlFor="search" className="flex ">
-            <input
-              type="search"
-              id="search"
-              name=""
-              className="outline-none text-slate-700 mr-2  "
-            />
-            <Search className="cursor-pointer" onClick={handleOpen} />
-          </label>
+      <form className="flex items-center" method="get" action="/">
+        {isChecked && (
+          <input
+            type="search"
+            id="search-checkbox"
+            name="search"
+            className="text-black p-1"
+            ref={htmlSearchField}
+          />
         )}
+        <button type="submit" onClick={handleIcon}>
+          <Search className="cursor-pointer"/>
+        </button>
       </form>
     </>
   );
