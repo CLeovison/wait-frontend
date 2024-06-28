81e2b3a03f0bf08ac4b0f0cf9fc9cbd14feb6db4
CLeovison <cleovison@gmail.com>
2024-06-28 23:32:31 +0800

    Done - Getting Data from Backend

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,30 +1,43 @@
 import React, { useRef, useState } from "react";
 import { Search } from "lucide-react";
 
-
 export default function SearchBar() {
   const [isChecked, setIsChecked] = useState(false);
-  const htmlSearchField = useRef(null)
-  
+  const htmlSearchField = useRef(null);
+
   //Fetching Data States
   const [isLoading, setIsLoading] = useState(false);
-  
-
+  const [query, setQuery] = useState([])
+  //Handle Functions
   const handleIcon = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     if (isChecked && htmlSearchField.current.value.length > 0) {
       // do search
-      e.target.closest('form').requestSubmit(e.currentTarget)
+      e.target.closest("form").requestSubmit(e.currentTarget);
     }
     setIsChecked((prev) => !prev);
   };
- 
- 
+  const handleSearch = async (request) => {
+    const url = 'http://localhost:5000/api';
 
+    try {
+      setIsLoading(true)
+      const response = await fetch(`${url}/products?products=${request}`)
+      const products = await response.json()
+      setQuery(products)
+    } catch (error) {
+      console.error("Search Error:", error);
+    }
+    setIsLoading(false)
+  };
+  if(isLoading){
+    return <div><p>Loading...</p></div>
+  }
 
+  //End of Handle Functions
   return (
     <>
-      <form className="flex items-center" method="get" >
+      <form className="flex items-center" method="get" action="http://localhost:5000/api/products?products=">
         {isChecked && (
           <input
             type="search"
@@ -32,19 +45,16 @@ export default function SearchBar() {
             name="search"
             className="text-black p-1"
             ref={htmlSearchField}
-          
-         
+            onChange={handleSearch}
+            placeholder="Search Product"
           />
-          
         )}
+
         <button type="submit" onClick={handleIcon}>
-          <Search className="cursor-pointer"/>
+          <Search className="cursor-pointer" />
         </button>
       </form>
-
- 
+        
     </>
-
-
   );
 }
