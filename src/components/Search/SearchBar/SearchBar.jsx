9620b93794abec29f2952fd09ef113d243b17ab9
Clark Leovison Rey <cleovison@gmail.com>
2024-07-08 21:32:32 +0800

    Conflict

--- src/components/Search/SearchBar/SearchBar.jsx
+++ src/components/Search/SearchBar/SearchBar.jsx
@@ -1,10 +1,10 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Search } from "lucide-react";
 import { useSearchParams } from "react-router-dom";
 
 export default function SearchBar() {
   const [isChecked, setIsChecked] = useState(false);
-
+  const [query, setQuery] = useState([])
   const [searchParams, setSearchParams] = useSearchParams({ q: "" });
   const htmlSearchField = useRef(null);
 
@@ -25,12 +25,36 @@ export default function SearchBar() {
 
   const handleValue = () => {
     searchParams.get("q");
+   
   };
+
+  useEffect(() =>{
+    
+  const handleSubmit = async () => {
+    const url = "http://localhost:5000/api"
+
+    try{
+      const response = await fetch(`${url}/products?term=${searchParams.get("term")}`)
+      if(!response){
+        throw new Error("The data that you are getting is not available")
+      }
+      const products = await response.json()
+      console.log(products)
+      
+      setQuery(products.productPaginated)
+    }catch(error){
+      console.error("Search error:", error)
+    }
+  };
+  handleSubmit()
+
+  },[])
+  
   //End of Handle Functions
 
   return (
     <>
-      <form className="flex items-center">
+      <form className="flex items-center" >
         {isChecked && (
           <input
             type="search"
@@ -40,8 +64,7 @@ export default function SearchBar() {
             ref={htmlSearchField}
             placeholder="Search Product"
             onChange={handleChange}
-            value={handleValue((e) => e.target.value)}
-           
+            value={handleValue(e => query(e.target.value))}
           />
         )}
 
@@ -49,6 +72,7 @@ export default function SearchBar() {
           <Search className="cursor-pointer" />
         </button>
       </form>
+      
     </>
   );
 }
