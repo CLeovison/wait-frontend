a7164eb15df8af8439a211d3e792303ef35feedd
Leovison <cleovison@gmail.com>
2024-07-10 09:00:23 +0800

    Changes

--- src/components/Search/SearchResult/SearchResult.jsx
+++ src/components/Search/SearchResult/SearchResult.jsx
@@ -1,11 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
-import { useSearchParams } from "react-router-dom";
+
 
 export default function SearchResult() {
   return (
     <>
-      {isLoading ? <p>Loading...</p> : ""}
+
 
     </>
   )
