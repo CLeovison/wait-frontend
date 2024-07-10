2cbdde0aedce4ac732c60243ebb4d7650d6d5d10
Leovison <cleovison@gmail.com>
2024-07-10 08:58:45 +0800

    .

--- src/components/Search/SearchResult/SearchResult.jsx
+++ src/components/Search/SearchResult/SearchResult.jsx
@@ -1,40 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
-import { useSearchParams } from "react-router-dom";
+
 
 export default function SearchResult() {
 
-  const [query, setQuery] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [searchParams] = useSearchParams();
-
-  useEffect(() => {
-    const handleChange = async () => {
-      const url = "http://localhost:5000/api";
-
-      try {
-        setIsLoading(true);
-        const response = await fetch(
-          `${url}/products?term=${searchParams.get("term")}`
-        );
-        if (!response) {
-          throw new Error("The Data That You Are Retriving is Not Available");
-        }
-        const products = await response.json();
-        setQuery(products);
-      } catch (error) {
-        console.error("Search Error", error);
-        setQuery([]);
-      }
-      setIsLoading(false);
-    };
-    handleChange();
-  }, []);
 
   return (
     <>
-      {isLoading ? <p>Loading...</p> : ""}
-
+    
     </>
   );
 }
