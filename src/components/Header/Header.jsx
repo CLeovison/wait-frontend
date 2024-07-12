0e6427892960f5e97b33c0421ff8beeb16cb8ca3
Leovison <cleovison@gmail.com>
2024-07-12 17:26:55 +0800

    Modified The Search Result

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import { UserRound } from "lucide-react";
 import { ShoppingCart } from "lucide-react";
 import SearchBar from "../Search/SearchBar/SearchBar";
+import SearchResult from "../Search/SearchResult/SearchResult";
 export default function Header() {
   return (
     <>
@@ -11,6 +12,7 @@ export default function Header() {
         <ul className="flex">
           <li className="p-4 text-white inline-block">
             <SearchBar />
+            <SearchResult />
           </li>
           <li className="p-4 text-white grid content-center">
             <Link>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Slider from "../../components/Slider/Slider";
-import SearchResult from "../../components/Search/SearchResult/SearchResult";
+
 
 export default function Home() {
   return (
     <>
       <Slider />
-      <SearchResult />
+ 
     </>
   );
 }
