d83a4fe428d62d1ebd17d32f208ad46bdcc8f41d
Clark Leovison Rey <cleovison@gmail.com>
2024-07-16 00:08:17 +0800

    .

--- src/hooks/Context/SearchContext.jsx
+++ src/hooks/Context/SearchContext.jsx
@@ -1,26 +1,29 @@
-import React, { createContext, useState } from 'react'
+import React, { createContext, useState } from "react";
 
 const SearchContext = createContext();
 
+export const SearchProvider = ({ children }) => {
+  const [query, setQuery] = useState("");
+  const [result, setResult] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-export const SearchProvider = ({children}) =>{
-    const [query, setQuery] = useState('')
-    const [result, setResult] = useState([])
-    const [isLoading, setIsLoading] = useState(false)
+  const search = async () => {
+    const url = "http://localhost:5000";
 
-    const search = async ()=>{
-        const url = 'http://localhost:5000'
+    try {
+      setIsLoading(true);
 
-        try{
-            setIsLoading(true)
-        }catch(error){
-            console.error("Search Term:", error);
-        }
-        
+      const response = await fetch(`${url}`);
+      if (!response) {
+        throw new Error("No Result Was Found");
+      }
+      const products = await products.json();
+      setResult(products);
+    } catch (error) {
+      console.error("Search Term:", error);
+      setResult([])
     }
+  };
 
-    return (
-        <>
-        </>
-    )
-}
+  return <></>;
+};
