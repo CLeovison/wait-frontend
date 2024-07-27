db20c99e9ba96236b1c7e44a21c596617359c807
Clark Leovison Rey <cleovison@gmail.com>
2024-07-27 23:50:14 +0800

    Seperate the .get in Context

--- src/hooks/Context/SearchContext.jsx
+++ src/hooks/Context/SearchContext.jsx
@@ -4,36 +4,41 @@ import { useSearchParams } from "react-router-dom";
 export const SearchContext = createContext("");
 
 export const SearchContextProvider = ({ children }) => {
+  //States
   const [result, setResult] = useState([]);
   const [query, setQuery] = useState();
-  const [searchParams, setSearchParams] = useSearchParams();
-  const [isLoading, setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false);
 
+  //SearchParams
+  const [searchParams, setSearchParams] = useSearchParams();
+  const searchValue = searchParams.get("search");
 
   //UseEffect
   useEffect(() => {
     const search = async () => {
       const url = "http://localhost:5000/api";
 
-      try{
-        setIsLoading(true)
-        const response = await fetch(`${url}/search/term=${searchParams.get('search')}`)
-        if(!response){
-          throw new Error("The product that you are retriving doesn't exist")
+      try {
+        setIsLoading(true);
+        const response = await fetch(`${url}/search/term=${searchValue}`);
+        if (!response) {
+          throw new Error("The product that you are retriving doesn't exist");
         }
         const products = await response.json();
-        setResult(products.productinfo)
-      }catch(error){
-        console.error("Search Error:",error)
-        setResult([])
+        setResult(products.productinfo);
+      } catch (error) {
+        console.error("Search Error:", error);
+        setResult([]);
       }
-      setIsLoading(false)
+      setIsLoading(false);
     };
     search();
   }, []);
   return (
     <>
-      <SearchContext.Provider value={{ result, query, searchParams,isLoading }}>
+      <SearchContext.Provider
+        value={{ result, query, searchParams, isLoading }}
+      >
         {children}
       </SearchContext.Provider>
     </>
