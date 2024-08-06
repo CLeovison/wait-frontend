b848ddf5d47de0d7d22c323f081aedc5cff0a1ba
Clark Leovison Rey <cleovison@gmail.com>
2024-08-06 23:45:46 +0800

    Added Contents on Card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import { useSearch } from "../../hooks/Context/useSearch";
 
 export default function Card() {
+  const { result } = useSearch();
   return (
     <>
       <section className="border-black">
-        <img src="" alt="" />
-        <h2 className="">Redemption Campaign</h2>
-        <p></p>
+        {result.map(item => <li key={item._id}>
+          <h1>{item.productinfo?.productname}</h1>
+          <p>{item.productinfo?.description}</p>
+        </li>)}
       </section>
     </>
   );
--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import SearchResult from '../../components/Search/SearchResult/SearchResult'
+import Card from '../../components/Card/Card'
 
 
 
@@ -8,7 +9,7 @@ export default function Products() {
   return (
     <>
    <SearchResult/>
-
+    <Card/>
     </>
   )
 }
