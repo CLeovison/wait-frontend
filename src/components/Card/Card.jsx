fa8431ec1a9e650a0e367e520cd1eb5071e19df0
Clark Leovison Rey <cleovison@gmail.com>
2024-08-08 22:26:41 +0800

    Doing the Card Component

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -5,11 +5,12 @@ export default function Card() {
   const { result } = useSearch();
   return (
     <>
-      <section className="border-black">
-        {result.map(item => <li key={item._id}>
-          <h1>{item.productinfo?.productname}</h1>
-          <p>{item.productinfo?.description}</p>
-        </li>)}
+      <section className=" border-r w-80 p-1">
+        <div className="card-image">
+          {result.map((images) => (
+            <p key={images._id}>{images.imageUrl}</p>
+          ))}
+        </div>
       </section>
     </>
   );
