e3770d8fc4e9ce3405e18789a5a06828bc1d9e35
Clark Leovison Rey <cleovison@gmail.com>
2024-08-09 23:57:16 +0800

    Changes on the Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
 import { Link } from 'react-router-dom'
-
 export default function Footer() {
   return (
         <>
-        <Link to="/"></Link>
+        <div className=" mt-80 pt-14 flex justify-evenly items-center border-t-2 border-t-zinc-100">
+          <Link to="https://www.facebook.com/waitclothing">Facebook</Link>
+          <Link to="https://www.instagram.com/waitclothing">Instagram</Link>
+        </div>
         </>
   )
 }
--- src/components/Sider/Sider.jsx
+++ src/components/Sider/Sider.jsx
@@ -4,10 +4,11 @@ import { Home } from 'lucide-react'
 import { Shirt } from 'lucide-react'
 import { UsersRound } from 'lucide-react'
 import { SquareUser } from 'lucide-react'
+import Footer from '../Footer/Footer'
 export default function Sider() {
   return (
     <>
-        <aside className='bg-zinc-700 w-56 '>
+        <aside className='bg-zinc-700 w-56  '>
         <div className="logo">
                 <Link><img src="/src/assets/logo/wait.png" alt=""  className='w-28 ml-3 mt-1 mb-3'/></Link>      
         </div>
@@ -18,7 +19,10 @@ export default function Sider() {
                 <li className="p-4"><Link to="/contact" className='text-white flex flex-row items-center'><SquareUser className="mr-2.5"/>Contact</Link></li>
             </ul>
         <footer>
-         
+
+          <div>
+          <Footer />
+          </div>
         </footer>    
         </aside>
         
