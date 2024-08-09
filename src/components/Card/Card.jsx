4ebde5ffd5e4f4fa135de0e2b0f803ef4e787b84
Clark Leovison Rey <cleovison@gmail.com>
2024-08-10 01:01:43 +0800

    Modified Card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { useSearch } from "../../hooks/Context/useSearch";
 
-export default function Card() {
+export default function Card({props}) {
   const { result } = useSearch();
   return (
     <>
       <section className=" border-r w-80 p-1">
         <div className="card-image">
           {result.map((images) => (
-            <p key={images._id}>{images.imageUrl}</p>
+            <li key={images._id}>
+              <img src="" alt="" />
+              <h2>{props.productname}</h2>
+              <p>{props.productdesc}</p>
+            </li>
           ))}
         </div>
       </section>
