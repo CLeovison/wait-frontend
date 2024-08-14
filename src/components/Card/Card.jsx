b3796af3d076445745d84cf04a0952f4d4de1d77
Clark Leovison Rey <cleovison@gmail.com>
2024-08-14 22:31:52 +0800

    Remove the Data in Card Component

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,19 +1,13 @@
 import React from "react";
 import { useSearch } from "../../hooks/Context/useSearch";
-import { Redemption } from "../../util/ImageCollection/Images";
+
 export default function Card() {
   const { result } = useSearch();
   return (
     <>
       <section className=" border-r w-80 p-1">
         <div className="card-image">
-          {result.map((data) => (
-            <li key={data._id}>
-              <img src={Redemption.PrimaryBlack} alt="" />
-              <h2>{data.productinfo?.productname}</h2>
-              <p>{data.productinfo?.productdesc}</p>
-            </li>
-          ))}
+ 
         </div>
       </section>
     </>
