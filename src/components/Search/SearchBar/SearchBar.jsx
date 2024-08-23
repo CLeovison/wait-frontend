2ae2189c7a8f3a3358c432413dfc3a4b87c21e94
Clark Leovison Rey <cleovison@gmail.com>
2024-08-23 23:36:09 +0800

    Added Fragments on Search Result

--- src/components/Search/SearchBar/SearchBar.jsx
+++ src/components/Search/SearchBar/SearchBar.jsx
@@ -8,25 +8,24 @@ export default function SearchBar() {
   const [searchTerm, setSearchTerm] = useState("");
   const [searchParams, setSearchParams] = useSearchParams();
   const navigate = useNavigate();
-  
-  
-  useEffect(()=>{
-    
-    if(searchParams.get("productinfo.productname") && searchParams.get("productinfo.productname") != "") {
-      setSearchTerm(searchParams.get("productinfo.productname"))
-      setIsChecked(true)
-    } else {
-      setIsChecked(false)
-    }
 
-  },[searchParams])
+  useEffect(() => {
+    if (
+      searchParams.get("productinfo.productname") &&
+      searchParams.get("productinfo.productname") != ""
+    ) {
+      setSearchTerm(searchParams.get("productinfo.productname"));
+      setIsChecked(true);
+    } else {
+      setIsChecked(false);
+    }
+  }, [searchParams]);
 
   const handleIcon = (e) => {
     e.preventDefault();
     if (isChecked) {
       e.target.closest("form").requestSubmit(e.currentTarget);
-      if(searchTerm.trim() == "")
-        setIsChecked(false)
+      if (searchTerm.trim() == "") setIsChecked(false);
     } else {
       setIsChecked(true);
     }
@@ -34,28 +33,32 @@ export default function SearchBar() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    navigate("/products")
-    if(searchTerm.trim() != "")
-      setSearchParams({"productinfo.productname": searchTerm})
+    navigate("/products");
+    if (searchTerm.trim() != "")
+      setSearchParams({ "productinfo.productname": searchTerm });
   };
 
   return (
-    <form className="flex items-center" onSubmit={handleSubmit}>
-      {isChecked && (
-        <input
-          type="search"
-          id="search-checkbox"
-          name="search"
-          className="text-black p-1"
-          ref={htmlSearchField}
-          placeholder="Search Product"
-          onChange={(event) => setSearchTerm(event.target.value)}
-          value={searchTerm}
-        />
-      )}
-      <button type="submit" onClick={handleIcon}>
-        <Search className="cursor-pointer" />
-      </button>
-    </form>
+    <>
+      <form className="flex items-center" onSubmit={handleSubmit}>
+        {isChecked && (
+          <input
+            type="search"
+            id="search-checkbox"
+            name="search"
+            className="text-black p-1"
+            ref={htmlSearchField}
+            placeholder="Search Product"
+            onChange={(event) => setSearchTerm(event.target.value)}
+            value={searchTerm}
+          />
+        )}
+        <button type="submit" onClick={handleIcon}>
+          <Search className="cursor-pointer" />
+        </button>
+      </form>
+
+  
+    </>
   );
 }
