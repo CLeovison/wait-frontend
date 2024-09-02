6bd0989f6adc904f2e2e03feee7992e7816fb2e5
Clark Leovison Rey <cleovison@gmail.com>
2024-09-03 00:01:16 +0800

    Remove ImageCollection

--- src/util/ImageCollection/Images.js
+++
@@ -1,9 +0,0 @@
-import BlackPrime from "../../assets/collections/2023 Summer/Redemption Collection/Black/Primary.jpg"
-import BlackSecond from "../../assets/collections/2023 Summer/Redemption Collection/Black/Secondary.jpg"
-
-
-export const Redemption = {
-    PrimaryBlack: BlackPrime,
-    SecondaryBlack: BlackSecond,
-    
-}
