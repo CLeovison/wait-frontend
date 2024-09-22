fbee6a0b9d9cdcf733b203516602b9fce73e015a
Clark Leovison Rey <cleovison@gmail.com>
2024-09-23 00:16:43 +0800

    feat: Added A Link/Button on Card Component

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Card({ cards }) {
   return (
@@ -8,9 +9,11 @@ export default function Card({ cards }) {
           src={`http://localhost:5000/api/uploads/${
             cards.image?.filename || "default.png"
           }`}
-          alt={cards.productinfo.productname}
+          alt={cards.productinfo.productname || "No Image"}
         />
         <p>{cards.productinfo.productname}</p>
+        <p>{cards.productinfo.price}</p>
+        <Link to="/">Brix</Link>
       </section>
     </>
   );
